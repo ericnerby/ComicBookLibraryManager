ef76809c7d2bfa92ed25b95d43bf45427e33af66
Eric Nerby <ericnerby@msn.com>
2021-04-02 11:11:10 -0400

    moved functionality into ComicBookArtistsRepository

--- ComicBookShared/Data/Repository.cs
+++ ComicBookShared/Data/Repository.cs
@@ -1,7 +1,6 @@
 ﻿using ComicBookShared.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,42 +16,19 @@ namespace ComicBookShared.Data
             _context = context;
         }
 
-        public IList<Series> GetSeriesList()
-        {
-            return _context.Series.OrderBy(s => s.Title).ToList();
-        }
-
         public IList<Artist> GetArtists()
         {
             return _context.Artists.OrderBy(a => a.Name).ToList();
         }
 
+        public IList<Series> GetSeriesList()
+        {
+            return _context.Series.OrderBy(s => s.Title).ToList();
+        }
+
         public IList<Role> GetRoles()
         {
             return _context.Roles.OrderBy(r => r.Name).ToList();
-        }
-
-        public ComicBookArtist GetComicBookArtist(int id)
-        {
-            return _context.ComicBookArtists
-                .Include(cba => cba.Artist)
-                .Include(cba => cba.Role)
-                .Include(cba => cba.ComicBook.Series)
-                .Where(cba => cba.Id == id)
-                .SingleOrDefault(); ;
-        }
-
-        public void AddComicBookArtist(ComicBookArtist comicBookArtist)
-        {
-            _context.ComicBookArtists.Add(comicBookArtist);
-            _context.SaveChanges();
-        }
-
-        public void DeleteComicBookArtist(int id)
-        {
-            var comicBookArtist = new ComicBookArtist() { Id = id };
-            _context.Entry(comicBookArtist).State = EntityState.Deleted;
-            _context.SaveChanges();
         }
     }
 }
