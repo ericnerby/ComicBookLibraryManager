ca58ad0ef18c180685922631e2bbd6229f569c72
Eric Nerby <ericnerby@msn.com>
2021-04-02 11:52:22 -0400

    implement BaseRepository

--- ComicBookShared/Data/ComicBooksRepository.cs
+++ ComicBookShared/Data/ComicBooksRepository.cs
@@ -8,27 +8,25 @@ using System.Threading.Tasks;
 
 namespace ComicBookShared.Data
 {
-    public class ComicBooksRepository
+    public class ComicBooksRepository : BaseRepository<ComicBook>
     {
-        private Context _context = null;
-
         public ComicBooksRepository(Context context)
+            : base(context)
         {
-            _context = context;
         }
 
-        public IList<ComicBook> GetList()
+        public override IList<ComicBook> GetList()
         {
-            return _context.ComicBooks
+            return Context.ComicBooks
                 .Include(cb => cb.Series)
                 .OrderBy(cb => cb.Series.Title)
                 .ThenBy(cb => cb.IssueNumber)
                 .ToList();
         }
 
-        public ComicBook Get(int id, bool includeRelatedEntities = true)
+        public override ComicBook Get(int id, bool includeRelatedEntities = true)
         {
-            var comicBooks = _context.ComicBooks.AsQueryable();
+            var comicBooks = Context.ComicBooks.AsQueryable();
             if (includeRelatedEntities)
             {
                 comicBooks = comicBooks
@@ -41,51 +39,9 @@ namespace ComicBookShared.Data
                 .SingleOrDefault();
         }
 
-        public void Add(ComicBook comicBook)
-        {
-            _context.ComicBooks.Add(comicBook);
-
-            if (comicBook.Series != null && comicBook.Series.Id > 0)
-            {
-                _context.Entry(comicBook.Series).State = EntityState.Unchanged;
-            }
-
-            foreach (ComicBookArtist artist in comicBook.Artists)
-            {
-                if (artist.Artist != null && artist.Artist.Id > 0)
-                {
-                    _context.Entry(artist.Artist).State = EntityState.Unchanged;
-                }
-
-                if (artist.Role != null && artist.Role.Id > 0)
-                {
-                    _context.Entry(artist.Role).State = EntityState.Unchanged;
-                }
-            }
-
-            _context.SaveChanges();
-        }
-
-        public void Update(ComicBook comicBook)
-        {
-            _context.ComicBooks.Attach(comicBook);
-            var comicBookEntry = _context.Entry(comicBook);
-            comicBookEntry.State = EntityState.Modified;
-
-            _context.SaveChanges();
-        }
-
-        public void Delete(int id)
-        {
-            var comicBook = new ComicBook() { Id = id };
-            _context.Entry(comicBook).State = EntityState.Deleted;
-
-            _context.SaveChanges();
-        }
-
         public bool ComicBookSeriesHasIssueNumber(int id, int seriesId, int issueNumber)
         {
-            return _context.ComicBooks
+            return Context.ComicBooks
                 .Any(cb => cb.Id != id &&
                            cb.SeriesId == seriesId &&
                            cb.IssueNumber == issueNumber);
@@ -93,7 +49,7 @@ namespace ComicBookShared.Data
 
         public bool ComicBookHasArtistRoleCombination(int comicBookId, int artistId, int roleId)
         {
-            return _context.ComicBookArtists
+            return Context.ComicBookArtists
                 .Any(cba => cba.ComicBookId == comicBookId &&
                             cba.ArtistId == artistId &&
                             cba.RoleId == roleId);
